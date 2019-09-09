f917f61e9138d6ff41bd5f0738a2cf44fed06620
Lauren Musante and Mike McShane <lauren@>
2019-09-09 15:38:05 -0700

    added clearInterval function

--- src/main.js
+++ src/main.js
@@ -8,21 +8,30 @@ $(document).ready(function() {
   $("form#bear-form").submit(function(event) {
     event.preventDefault();
 
+    $('form#bear-form').trigger("reset");
+
     let bearName = $('#inputted-name').val();
     let newBear = new FuzzyWuzzy(bearName);
     console.log(newBear);
 
     $('.begin-game').show();
+    $('.game-over').hide();
 
     newBear.setHunger();
     newBear.setMood();
     newBear.setEnergy();
 
-    setInterval(function(){
+    let id = setInterval(function(){
       $('#food-display').text(newBear.foodLevel);
+      if (newBear.foodLevel === 0) {
+        $('.begin-game').hide();
+        $('#bear-form').show();
+        $('.game-over').show();
+        clearInterval(id);
+      }
       $('#mood-display').text(newBear.moodLevel);
       $('#energy-display').text(newBear.energyLevel);
-    }, 500);
+    }, 0);
 
     $("button#feed-button").click(function(event) {
       newBear.feed();
@@ -35,10 +44,6 @@ $(document).ready(function() {
     $("button#sleep-button").click(function(event) {
       newBear.sleep();
     });
-
-    if (newBear.foodLevel === 0) {
-      alert("you lose idiot");
-    }
 
   });
 });
